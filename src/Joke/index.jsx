6a05765c12dedd2b0d96c97d5589492fcd097df5
SlavkaVavrinova <slavka.vav@gmail.com>
2021-05-22 12:42:48 +0200

    Doplnění funkčnosti tlačítek k úkolu 14, ale už s nastavením počátečního stavu dle jokes.js z úkolu 15

--- src/Joke/index.jsx
+++ src/Joke/index.jsx
@@ -1,26 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './style.css';
 
 export const Joke = (props) => {
+  const [likes, setLikes] = useState(props.likes);
+  const [dislikes, setDislikes] = useState(props.dislikes);
+
+  const handleClickUp = () => {
+    setLikes(likes + 1);
+  };
+
+  const handleClickDown = () => {
+    setDislikes(dislikes + 1);
+  };
+
   return (
     <div class="joke">
       <div class="joke__body">
         <div class="joke__user">
-          <img class="user-avatar" src={props.userAvatar}/>
+          <img class="user-avatar" src={props.userAvatar} />
           <p class="user-name">{props.userName}</p>
         </div>
 
         <p class="joke__text">{props.text}</p>
       </div>
       <div class="joke__likes">
-        <button id="btn-up" class="btn-like btn-like--up"></button>
+        <button
+          id="btn-up"
+          class="btn-like btn-like--up"
+          onClick={handleClickUp}
+        ></button>
         <span id="likes-up" class="likes-count likes-count--up">
-          {props.likes}
+          {likes}
         </span>
-        <button id="btn-down" class="btn-like btn-like--down"></button>
+        <button
+          id="btn-down"
+          class="btn-like btn-like--down"
+          onClick={handleClickDown}
+        ></button>
         <span id="likes-down" class="likes-count likes-count--down">
-          {props.dislikes}
+          {dislikes}
         </span>
       </div>
     </div>
